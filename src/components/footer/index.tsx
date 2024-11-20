13af71169d3428cf063a54dc510fa2c6a800cddd
Gountzjs <tomasjsdev@gmail.com>
2024-11-20 08:52:43 -0300

    feat: :lipstick: footer rediseño colores

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export function Footer() {
 	return (
-		<footer className="bg-gray-200 dark:bg-gray-900 text-red-600 flex flex-col items-center justify-center p-4 gap-2 font-semibold h-fit w-full">
+		<footer className="bg-neutral-300 dark:bg-black text-gray-950 dark:text-neutral-50 flex flex-col items-center justify-center p-4 gap-2 font-semibold h-fit w-full">
 			<p className="w-fit">
 				Inspirado en:{" "}
 				<Link
 					href="https://manz.dev/manzcards/"
 					target="_black"
-					className="text-yellow-500"
+					className="text-t1-secondary dark:text-t1-secondary"
 				>
 					MANZDEV
 				</Link>{" "}
@@ -16,7 +16,7 @@ export function Footer() {
 				<Link
 					href="https://cards.uxanarangel.com"
 					target="_black"
-					className="text-yellow-500"
+					className="text-t1-secondary dark:text-t1-secondary"
 				>
 					ANARANGEL
 				</Link>
