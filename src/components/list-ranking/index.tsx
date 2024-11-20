d7aa3e9d3ab447fefaf1942d727b3e00e76e98e4
Gountzjs <tomasjsdev@gmail.com>
2024-11-20 13:26:50 -0300

    refactor: :zap: listranking envolver previewranking en li

--- src/components/list-ranking/index.tsx
+++ src/components/list-ranking/index.tsx
@@ -9,13 +9,14 @@ export function ListRanking() {
 	return (
 		<ul className="w-[600px] max-w-[80vw]">
 			{data.map((user) => (
-				<PreviewRanking
-					key={user.id}
-					username={user.username}
-					id={user.id}
-					rank={rankMapper(user.rank)}
-					quantity={user.cards.length}
-				/>
+				<li key={user.id}>
+					<PreviewRanking
+						username={user.username}
+						id={user.id}
+						rank={rankMapper(user.rank)}
+						quantity={user.cards.length}
+					/>
+				</li>
 			))}
 		</ul>
 	);
