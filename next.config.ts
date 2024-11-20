381eb857d721fcf217618e49db3bc4e0859caa53
Gountzjs <tomasjsdev@gmail.com>
2024-11-20 08:46:26 -0300

    fix: :building_construction: agregar domain images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	basePath: "/embee-cards",
+	images: {
+		domains: ["static-cdn.jtvnw.net"],
+		remotePatterns: [],
+	},
 };
 
 export default nextConfig;
