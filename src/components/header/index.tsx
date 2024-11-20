9b7f34e77d5f495b15147a65790b86a899923779
Gountzjs <tomasjsdev@gmail.com>
2024-11-20 08:58:45 -0300

    feat: :sparkles: integrar titlepage header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import { BASE_HREF } from "@/consts/url";
 import Image from "next/image";
 import { Navbar } from "../navbar";
+import { TitlePage } from "../title-page";
 
 export function Header() {
 	return (
@@ -8,14 +9,13 @@ export function Header() {
 			<Navbar />
 			<div className="flex items-center justify-center gap-6">
 				<Image
-					src={`${BASE_HREF}/imgs/abejajayz.png`}
+					className="rounded-full"
+					src={`${BASE_HREF}/imgs/avatar-embee.jpg`}
 					width={100}
 					height={100}
-					alt="Avatar abejajayz"
+					alt="Avatar embeejayz"
 				/>
-				<h1 className="font-extrabold font-sans text-3xl text-red-600">
-					Embeejayz cartas
-				</h1>
+				<TitlePage />
 			</div>
 		</header>
 	);
