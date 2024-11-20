797b480493a0097742368a49f10f8608dc861a8d
Gountzjs <tomasjsdev@gmail.com>
2024-11-20 09:19:44 -0300

    build: :ambulance: agregar static path ranking user id

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	basePath: "/embee-cards",
+	assetPrefix: "/embee-cards/",
 	images: {
 		domains: ["static-cdn.jtvnw.net"],
 		remotePatterns: [],
--- src/app/ranking/[id]/page.tsx
+++ src/app/ranking/[id]/page.tsx
@@ -1,9 +1,22 @@
 import { BasePage } from "@/components/base-page";
 import { UserDetail } from "@/components/user-detail";
+import { User } from "@/models/interfaces";
+import { readFile } from "node:fs/promises";
+import path from "node:path";
 
 type Props = {
 	params: Promise<{ id: string }>;
 };
+
+export async function generateStaticParams() {
+	const filePath = path.join(process.cwd(), "public", "data", "users.json");
+	const fileContents = await readFile(filePath, "utf-8");
+	const { users } = JSON.parse(fileContents);
+
+	return users.map((user: User) => ({
+		id: user.id,
+	}));
+}
 
 export default async function UserIdPage({ params }: Props) {
 	const { id } = await params;
