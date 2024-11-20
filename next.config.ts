110aa0a94648848ca7d657f7fe99a17af8edaa65
Gountzjs <tomasjsdev@gmail.com>
2024-11-20 09:23:45 -0300

    fix: :bug: remover assets prefijo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	basePath: "/embee-cards",
-	assetPrefix: "/embee-cards/",
 	images: {
 		domains: ["static-cdn.jtvnw.net"],
 		remotePatterns: [],
