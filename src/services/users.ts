8b6cd704267c3f0bee2e56a753d39af19aabb69c
Gountzjs <tomasjsdev@gmail.com>
2024-11-20 09:00:52 -0300

    fix: :ambulance: agregar getusersranking service

--- src/services/users.ts
+++ src/services/users.ts
@@ -40,3 +40,39 @@ export const getUsersRanking = async (): Promise<{ users: UserRank[] }> => {
 		users: sortedUsers,
 	};
 };
+
+export const getUserById = async ({
+	id,
+}: {
+	id: string;
+}): Promise<{ user: UserRank }> => {
+	const { users } = await getUsersData();
+
+	const sortedUsers = users.sort(
+		(a, b) => b.cards.length - a.cards.length
+	) as UserRank[];
+
+	let rank = 0;
+
+	for (let i = 0; i < sortedUsers.length; i++) {
+		if (
+			i > 0 &&
+			sortedUsers[i].cards.length === sortedUsers[i - 1].cards.length
+		) {
+			sortedUsers[i].rank = rank;
+			continue;
+		}
+		rank = rank + 1;
+		sortedUsers[i].rank = rank;
+	}
+
+	const user = sortedUsers.find((user) => user.id === id) as UserRank;
+
+	if (!user) throw new Error("User not found");
+
+	user.rank = sortedUsers.find((user) => user.id === id)!.rank;
+
+	return {
+		user,
+	};
+};
