2b375b922849d52200f529d876da2e2445fa4c1e
Gountzjs <tomasjsdev@gmail.com>
2024-11-20 13:25:48 -0300

    feat: :sparkles: crear usecardsbyuser hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
-export * from "./useTheme.hook";
-export * from "./useUserRank.hook";
-export * from "./useUsersRank.hook";
+export * from './useCardsByUser.hook';
+export * from './useTheme.hook';
+export * from './useUserRank.hook';
+export * from './useUsersRank.hook';
