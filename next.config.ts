713baea4ebb61373f08b1640d7089124b342eaaf
Gountzjs <tomasjsdev@gmail.com>
2024-11-20 13:14:52 -0300

    build: :sparkles: agregar dominio imagen nextjsconfig

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	basePath: "/embee-cards",
 	images: {
-		domains: ["static-cdn.jtvnw.net"],
+		domains: [
+			"static-cdn.jtvnw.net",
+			"media1.giphy.com",
+			"cdna.artstation.com",
+		],
 		remotePatterns: [],
 	},
 };
