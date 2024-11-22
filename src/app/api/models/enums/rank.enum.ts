327ca14166a615906a964241b07a51bc5209ddae
Gountzjs <tomasjsdev@gmail.com>
2024-11-22 16:42:21 -0300

    feat: :sparkles: agregar spells dinamicos

--- src/app/api/models/enums/rank.enum.ts
+++ src/app/api/models/enums/rank.enum.ts
@@ -2,7 +2,7 @@ export enum Rank {
 	Challenger = "CHALLENGER",
 	Master = "MASTER",
 	Diamond = "DIAMOND",
-	Platinum = "PLATINUM",
+	Platinum = "PLATINIUM",
 	Gold = "GOLD",
 	Silver = "SILVER",
 	Bronze = "BRONZE",
--- src/app/shared/components/border-detail/index.tsx
+++ src/app/shared/components/border-detail/index.tsx
@@ -7,7 +7,30 @@ type Props = {
 	img: string;
 };
 
+const getSpellLeftClass = (rank: Rank): string => {
+	let spellLeftClass = "absolute aspect-square";
+	if (rank === Rank.Diamond || rank === Rank.Platinum) {
+		spellLeftClass += " bottom-[26px] left-[75px] aspect-square";
+		return spellLeftClass;
+	}
+	spellLeftClass += " bottom-[22px] left-[76px] aspect-square";
+	return spellLeftClass;
+};
+
+const getSpellRightClass = (rank: Rank): string => {
+	let spellRightClass = "absolute aspect-square";
+	if (rank === Rank.Diamond || rank === Rank.Platinum) {
+		spellRightClass += " bottom-[26px] left-[116px] aspect-square";
+		return spellRightClass;
+	}
+	spellRightClass += " bottom-[22px] left-[118px] aspect-square";
+	return spellRightClass;
+};
+
 export function BorderDetail({ rank, img, avatar, username }: Props) {
+	const spellLeftClass = getSpellLeftClass(rank);
+	const spellRightClass = getSpellRightClass(rank);
+
 	return (
 		<div
 			className={`flex flex-col items-center w-[250px] aspect-[5/9] pt-4 relative`}
@@ -24,6 +47,20 @@ export function BorderDetail({ rank, img, avatar, username }: Props) {
 				alt={`Borde rango ${rank.toLowerCase()}`}
 				width={250}
 				height={450}
+			/>
+			<img
+				className={spellLeftClass}
+				src="/summoner-spell/flash.jpg"
+				width={34.8}
+				height={34.8}
+				alt="Spell left"
+			/>
+			<img
+				className={spellRightClass}
+				src="/summoner-spell/ignite.jpg"
+				width={34.8}
+				height={34.8}
+				alt="Spell right"
 			/>
 			<img
 				className="absolute bottom-[13px] left-[17px]"
