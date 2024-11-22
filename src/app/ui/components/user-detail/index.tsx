06aefc280e3d37e11bcf75993656992ff19c96a2
Gountzjs <tomasjsdev@gmail.com>
2024-11-22 01:38:45 -0300

    refactor: :lipstick: rediseñar componente user detail

--- src/app/ui/components/user-detail/index.tsx
+++ src/app/ui/components/user-detail/index.tsx
@@ -10,8 +10,8 @@ export function UserDetail({ id }: Props) {
 	const { data } = useUserRankHook({ id });
 
 	return (
-		<article className="flex items-center outline outline-4 outline-t1-secondary py-12 px-4 rounded-lg gap-4 h-fit w-full relative">
-			<LinkBack url="/ranking" className="absolute top-4 right-4" />
+		<article className="flex flex-col items-center outline outline-4 outline-t1-secondary py-12 px-4 rounded-lg gap-4 h-fit w-[350px] relative">
+			<LinkBack url="/ranking" className="absolute top-4 left-4" />
 			{data && (
 				<div className="flex flex-col gap-4 items-center w-fit h-full">
 					<img
@@ -27,21 +27,21 @@ export function UserDetail({ id }: Props) {
 				</div>
 			)}
 			{data && (
-				<div className="flex flex-col items-center justify-between font-sans font-semibold gap-4 h-full w-full">
+				<div className="flex items-center justify-evenly font-sans font-semibold gap-4 h-full w-full">
+					<p className="text-xl">Rango</p>
 					<img
 						src={`/icons/rank/${rankMapper(data.rank)}.webp`}
 						alt="Leaderboard"
 						width={80}
 						height={80}
 					/>
-					<p className="text-xl">Rango</p>
 				</div>
 			)}
 
 			{data && (
-				<div className="flex flex-col items-center justify-between font-sans text-xl font-semibold gap-4 h-full w-full">
-					<p className="font-serif font-normal">{data.cards.length} / 125</p>
+				<div className="flex items-center justify-evenly font-sans text-xl font-semibold gap-4 h-full w-full">
 					<p>Cartas</p>
+					<p className="font-serif font-normal">{data.cards.length} / 125</p>
 				</div>
 			)}
 		</article>
